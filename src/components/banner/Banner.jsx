201582866aff5481652a65e88688cdcc4978a67d
Denise Oliveira <goliveira.denise@gmail.com>
2022-04-15 19:23:01 -0300

    feat: ajustes style  banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,15 +1,43 @@
 // import LinkButton from "../LinkButton/LinkButton";
 import styles from "./Banner.module.css";
 
+import { Features } from "./Icons";
+
+import { Icon } from "@chakra-ui/react";
+
+import { BiWifi, BiRestaurant, BiCar, BiCalendarEvent } from "react-icons/bi";
+import { FaConciergeBell, FaHotel, FaSwimmingPool} from "react-icons/fa";
+
 function Banner() {
-  return(
-    <section className={styles.banner_container}>
-      <h1>Bem-vindo ao <span>Otaku's Hotel</span></h1>
-      <p>Texto a definir</p>
+  return (
+    <div className={styles.banner_container}>
+      <div className={styles.banner_texto}>
+        <h1>
+          Bem-vindo ao <span>Otaku's Hotel</span>
+        </h1>
+        <p>
+          Lorem ipsum venenatis justo aliquam ultrices primis mi sociosqu,
+          tortor dolor aptent aliquam felis himenaeos felis pretium quisque,
+          sodales praesent tempor vulputate fringilla rhoncus justo. congue
+          blandit curae aptent quam leo ut accumsan, porttitor volutpat
+          tristique cursus lacus vestibulum magna, torquent justo leo rhoncus
+          sem turpis. 
+        </p>
+      </div>
+
       {/* <a href="/">Conferir agora</a> */}
       {/* <LinkButton to="/" text="/" /> */}
-      <icon></icon>
-    </section>
+
+      <div className={styles.banner_icons}>
+        <Features icon={<Icon as={FaConciergeBell} />} />
+        <Features icon={<Icon as={FaHotel} />} />
+        <Features icon={<Icon as={FaSwimmingPool} />} />
+        <Features icon={<Icon as={BiWifi} />} />
+        <Features icon={<Icon as={BiCar} />} />
+        <Features icon={<Icon as={BiRestaurant} />} />
+        <Features icon={<Icon as={BiCalendarEvent} />} />
+      </div>
+      </div>
   );
 }
 
