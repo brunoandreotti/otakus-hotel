e2b977086cc45387f63fc24648352fd339ff5636
Denise Oliveira <goliveira.denise@gmail.com>
2022-04-14 23:42:57 -0300

    feat: editando banner e linkbutton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Banner from "./components/banner/Banner";
 
 function App() {
   return (
     <>
+    <Banner />
     </>
   );
 }
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+// import LinkButton from "../LinkButton/LinkButton";
+import styles from "./Banner.module.css";
 
 function Banner() {
-  return (
-    <>
-    </>
+  return(
+    <section className={styles.banner_container}>
+      <h1>Bem-vindo ao <span>Otaku's Hotel</span></h1>
+      <p>Texto a definir</p>
+      {/* <a href="/">Conferir agora</a> */}
+      {/* <LinkButton to="/" text="/" /> */}
+      <icon></icon>
+    </section>
   );
 }
 
