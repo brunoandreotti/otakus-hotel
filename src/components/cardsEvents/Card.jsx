c8a3621dde03659fdbf89baea817266447fa2e59
Denise Oliveira <goliveira.denise@gmail.com>
2022-04-19 13:49:32 -0300

    feat: ajustes em components e page

--- src/components/cardsEvents/Card.jsx
+++ src/components/cardsEvents/Card.jsx
@@ -1,10 +1,11 @@
+import styles from "./Card.module.css";
 import CardText from "../cards/CardText";
 import events from "../../assets/images/events.jpg";
 import tea from "../../assets/images/tea.jpg";
 
 function Cards() {
   return (
-    <div>
+    <div className={styles.cards_container}>
       <CardText
         titulo={"Eventos"}
         texto={"Lorem ipsum egestas euismod diam, senectus faucibus quisque."}
