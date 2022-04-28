d8dc1a15057d625986a1e143df4114ffc8fb7ffc
Bruno Andreotti <brunoandreotti@outlook.com>
2022-04-28 12:09:39 -0300

    bug fix

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import FormsLayout from "../FormsLayout/FormsLayout.jsx";
-import LayoutInput from "../FormsLayout/LayoutInput.jsx";
+import FormsLayout from "../FormsLayout/FormsLayout.jsx"
+import LayoutInput from "../FormsLayout/LayoutInput.jsx"
+import InputRegister from "./InputRegister.jsx";
 function Register() {
-	return (
-		<section>
-			<FormsLayout TituloPage="Bem-Vindo!" />
-			<LayoutInput titulo="Faça seu cadastro" />
-		</section>
-	);
+    return (
+        <>
+            <FormsLayout TituloPage="Bem-Vindo!"/>
+            <LayoutInput titulo="Faça seu cadastro"/>
+            <InputRegister/>
+        </>
+    )
 }
 
 export default Register;
